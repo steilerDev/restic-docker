c9cf7fe942ef79e99d790a1e51d6509c72fd19ac
Frank Steiler <frank@steilerdev.de>
2022-04-10 10:03:37 +0000

    Bugfixing

--- rootfs/restic/status.sh
+++ rootfs/restic/status.sh
@@ -6,38 +6,37 @@
 # If called without status value it will pritty print the status which is read from file
 # Builds and exptects Influx Line Protocol (https://docs.influxdata.com/influxdb/latest/reference/syntax/line-protocol/)
 
-STATUS_FILE="/restic/status.info"
+#STATUS_FILE="/restic/status.info"
+STATUS_FILE="./status.info"
 
 MEASUREMENT_NAME="backup"
 FIELD_KEY_STATUS="status"
 FIELD_KEY_TIME="date"
 
+# This will parse the status either based on the provided input or from the file
 parse_status () {
-    if [ -z $STATUS ]; then
+    if [ ! -z $1 ] && [ ! -z $2 ]; then
+        STATUS=$1
+        DATE=$2
+    else
         if [ -f $STATUS_FILE ]; then
             DATA=$(cut $STATUS_FILE -c $(( ${#MEASUREMENT_NAME}+2 ))- | tr "," "\n")
             for DATA_POINT in $DATA ; do
                 if [[ $DATA_POINT == "$FIELD_KEY_STATUS="* ]]; then
-                    STATUS=$(echo $DATA_POINT | cut -c $(( ${#FIELD_KEY_STATUS}+2 ))-)
+                    STATUS=$(echo $DATA_POINT | cut -c $(( ${#FIELD_KEY_STATUS}+2 ))- | tr -d "\"")
                 elif [[ $DATA_POINT == "$FIELD_KEY_TIME="* ]]; then
                     DATE=$(echo $DATA_POINT | cut -c $(( ${#FIELD_KEY_TIME}+2 ))-)
                 fi
             done
         else
             STATUS="NO_STATUS"
-        fi
-    fi
-
-    if [ -z $DATE ]; then
-        if [ -f $STATUS_FILE ]; then
-            DATE=$(cat $STATUS_FILE)
-        else
             DATE=0
         fi
     fi
 
     declare -A STATUS_DICT
-    PARSED_DATE=$(date -d@"$(( $DATE / 1000000000 ))")
+    # DATE stored as Milliseconds, but `date` can only format seconds
+    PARSED_DATE=$(date -d@"$(( $DATE / 1000 ))")
     STATUS_DICT+=(
         ["NO_STATUS"]="No backup execution scheduled" 
         ["SCHEDULED"]="Backup execution scheduled, but not started" 
@@ -69,16 +68,16 @@ if [ -z $1 ] ; then
     echo -e "${BLUE}##  ${YELLOW}${STATUS}${BLUE} ##"
     echo -e "${BLUE}###############################################################################${STOP_COLOR}"
 else
-    if [ "$1" = "SCHEDULED"] && [ -f $STATUS_FILE ]; then
+    if [ "$1" = "SCHEDULED" ] && [ -f $STATUS_FILE ]; then
         echo "Not initializing status file, because it exists"
     else
         STATUS=$1
-        DATE=$(date +%s%N)
+        # Storing in Milliseconds (natively parsable DateTime for Grafana)
+        DATE=$(( $(date +%s%N) / 1000000 ))
         echo "${MEASUREMENT_NAME} ${FIELD_KEY_STATUS}=\"${STATUS}\",${FIELD_KEY_TIME}=${DATE}" > $STATUS_FILE
 
         echo "###############################################################################"
-        parse_status
-        #parse_status "$STATUS" "$DATE"
+        parse_status "$STATUS" "$DATE"
         echo "###############################################################################"
     fi
 fi
